e9ff6cfe339d52dea2d16fdc9b21de1d4ab97b88
Benjamin TD <benjamin.tdm@gmail.com>
2022-07-28 11:46:08 +0200

    switch styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,6 @@ const Home: NextPage = () => {
   const [displayedIsochrones, setDisplayedIsochrones] = useState<number | null>(
     null
   );
-  const { data: stationsData } = useSWR<StationsRes>("/api/stations");
   const { data: isochronesData } = useSWR<IsochronesRes>(
     hoveredStation ? `/api/isochrones/${hoveredStation}` : null
   );
@@ -26,7 +25,7 @@ const Home: NextPage = () => {
     if (map) return; // initialize map only once
     let mapboxMap = new mapboxgl.Map({
       container: mapContainer.current!,
-      style: "mapbox://styles/mapbox/light-v9",
+      style: "mapbox://styles/benjamintd/cl64tnf2g000814pdk237r6ij",
       center: [2, 45],
       zoom: 4,
     });
@@ -35,9 +34,10 @@ const Home: NextPage = () => {
       setMap(mapboxMap);
 
       mapboxMap.addSource("stations", {
-        type: "geojson",
-        data: { type: "FeatureCollection", features: [] },
+        type: "vector",
+        url: 'mapbox://benjamintd.4cgn60a2',
       });
+
       mapboxMap.addSource("isochrones", {
         type: "geojson",
         data: { type: "FeatureCollection", features: [] },
@@ -52,6 +52,7 @@ const Home: NextPage = () => {
           id: "stations",
           type: "circle",
           source: "stations",
+          "source-layer": "stations-apro5d",
           paint: {
             "circle-radius": 5,
             "circle-opacity": 0,
@@ -133,6 +134,7 @@ const Home: NextPage = () => {
           id: "stations-symbol",
           type: "symbol",
           source: "stations",
+          "source-layer": "stations-apro5d",
           layout: {
             "text-field": ["get", "name"],
             "text-offset": [0, -1.5],
@@ -174,10 +176,10 @@ const Home: NextPage = () => {
             layers: ["stations"],
           }
         );
-        if (features.length) {
+        if (features.length && mapboxMap.getSource("hoveredStation")) {
           mapboxMap.getCanvas().style.cursor = "crosshair";
           const station = features[features.length - 1]; // the largest according to the API scoring
-          setHoveredStation(station.id as number);
+          setHoveredStation(station.properties!.id as number);
           (mapboxMap.getSource("hoveredStation") as GeoJSONSource).setData(
             station
           );
@@ -192,25 +194,6 @@ const Home: NextPage = () => {
       });
     });
   }, [map]);
-
-  useEffect(() => {
-    if (map && stationsData?.stations) {
-      (map.getSource("stations") as GeoJSONSource).setData({
-        type: "FeatureCollection",
-        features: stationsData.stations.map((station) => ({
-          type: "Feature",
-          geometry: {
-            type: "Point",
-            coordinates: [station.longitudeE7 / 1e7, station.latitudeE7 / 1e7],
-          },
-          properties: {
-            name: station.name,
-          },
-          id: station.id,
-        })),
-      });
-    }
-  }, [stationsData, map]);
 
   useEffect(() => {
     if (displayedIsochrones === hoveredStation) {
@@ -248,27 +231,6 @@ const Home: NextPage = () => {
   return (
     <div className="relative w-screen h-screen">
       <InfoPanel />
-      {!stationsData && (
-        <div className="absolute flex items-center justify-center w-full h-full bg-gray-700 bg-opacity-20">
-          <svg
-            aria-hidden="true"
-            className="w-8 h-8 mr-2 text-white animate-spin fill-gray-900"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="currentColor"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentFill"
-            />
-          </svg>
-          <span className="sr-only">Loading...</span>
-        </div>
-      )}
       <div className="w-full h-full" ref={mapContainer} />
     </div>
   );
@@ -335,8 +297,7 @@ const InfoPanel = () => {
                       </p>
                       <p>
                         It is inspired by the great{" "}
-                        <a href="https://direkt.bahn.guru/">Direkt Bahn Guru</a>
-                        , from which the data is based.
+                        <a href="https://direkt.bahn.guru/">Direkt Bahn Guru</a>. The data is based off of this site, which sources it from the Deutsch Bahn.
                       </p>
                       <p>
                         Hover your mouse over a station to see the isochrones
