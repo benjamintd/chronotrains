d63313adc3d34f619d9775b12c105b55408c6714
Mateusz Sienkan <sin.mateusz@gmail.com>
2022-11-04 20:25:19 +0100

    Add Polish translation

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "fr", "de", "it", "ro", "ar", "nl", "tr"],
+    locales: ["en", "fr", "de", "it", "ro", "ar", "nl", "tr", "pl"],
   },
 };
